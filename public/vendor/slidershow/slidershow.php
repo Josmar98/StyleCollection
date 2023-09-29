57347599bcd439d42fe47f89947939703f1c9466
Josmar98 <josrod.2112@gmail.com>
2023-09-28 22:51:07 -0400

    First Commit

---
+++ public/vendor/slidershow/slidershow.php
@@ -0,0 +1,60 @@
+	<link rel="stylesheet" type="text/css" href="public/vendor/slidershow/css/estilos.css">
+	<link rel="stylesheet" type="text/css" href="public/vendor/slidershow/css/font-awesome.css">
+	<script type="text/javascript" src="public/vendor/slidershow/js/main.js"></script>
+	<style type="text/css">
+	
+	</style>
+	<div class="slideshow" style="">
+		<ul class="slider">
+			<li>
+				<img id="slideshow" src="public/assets/img/logo.jpg" alt="" >
+				<section class="caption">
+					<h3 class="titleslider">Cosmeticos Profesionales </h3>
+					<span class="contenidoslider">Descubre que mas ofrecemos</span>
+				</section>
+			</li>
+			
+			<li>
+				<img id="slideshow" src="public/assets/img/logo.jpg" alt="" >
+				<section class="caption">
+					<h3 class="titleslider"> Pre Lavados </h3>
+					<span class="contenidoslider">Hidratantes</span>
+				</section>
+			</li>
+			<li>
+				<img id="slideshow" src="public/assets/img/logo.jpg" alt="" >
+				<section class="caption">
+					<h3 class="titleslider"> Suavisante Capilar </h3>
+					<span class="contenidoslider"> Suaviza </span>
+				</section>
+			</li>
+			<li>
+				<img id="slideshow" src="public/assets/img/logo.jpg" alt="" >
+				<section class="caption">
+					<h3 class="titleslider"> Gel Mentolado </h3>
+					<span class="contenidoslider"> Relajante, Desinflamatorio, para Masajes</span>
+				</section>
+			</li>
+			<li>
+				<img id="slideshow" src="public/assets/img/logo.jpg" alt="" >
+				<section class="caption">
+					<h3 class="titleslider"> Mascarilla Capilar </h3>
+					<span class="contenidoslider"> Hidrata y Revitaliza </span>
+				</section>
+			</li>
+
+		</ul>
+
+		<!-- <ol class="pagination">
+			
+		</ol> -->
+	
+		<div class="left">
+			<span class="fa fa-chevron-left" style="text-shadow:0px 0px 10px #000;"></span>
+		</div>
+
+		<div class="right">
+			<span class="fa fa-chevron-right" style="text-shadow:0px 0px 10px #000;"></span>
+		</div>
+
+	</div>
